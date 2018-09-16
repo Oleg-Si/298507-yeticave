f114ba2251f09bfcabdb5fe66d1b02471e34e6df
OlegS <xwx18@mail.ru>
2018-09-16 12:45:15 +0300

    Выполнил 2 задание 2 часть

--- index.php
+++ index.php
@@ -44,6 +44,17 @@ $catalog = [
         'image' => 'img/lot-6.jpg'
     ]
 ];
+
+function format_price ($price) {
+    $new_price = ceil($price);
+    if ($new_price >= 1000) {
+        $new_price = number_format($new_price, 0, '.', ' ') . ' ₽';
+        return $new_price;
+    }
+    $new_price = $new_price . ' ₽';
+    return $new_price;
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -123,7 +134,7 @@ $catalog = [
                         <div class="lot__state">
                             <div class="lot__rate">
                                 <span class="lot__amount">Стартовая цена</span>
-                                <span class="lot__cost"><?php echo $catalog[$key]['price']; ?><b class="rub">р</b></span>
+                                <span class="lot__cost"><?php echo format_price($catalog[$key]['price']); ?></span>
                             </div>
                             <div class="lot__timer timer">
 
